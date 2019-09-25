a7c0e894fb197f61e02399b80782ad7690f144b1
Nisheal John <nishealajohn@karunya.edu.in>
2019-09-25 22:18:11 +0530

    remove initails

--- start.sh
+++ start.sh
@@ -1,18 +1,4 @@
-if [ ! -d "./vendor" ]; then
-  # If there is no ./vendor run install.sh.
-  sudo bash install.sh
-fi
-
-# Open the url
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        # Mac OSX
-        xdg-open http://localhost:8080/form.html
-
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-        open http://localhost:8080/form.html
-fi
-
+sudo bash install.sh
 php -S localhost:8080
 
 
